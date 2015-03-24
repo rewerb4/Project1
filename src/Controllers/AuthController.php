6d663a9ae5dc657c2701dd43663a2780439f221d
mat brewer <mathewbrewer@mail.weber.edu>
2015-03-23 19:34:48 -0600

    new project 1

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -12,6 +12,12 @@
 
 namespace Controllers;
 
+use Common\Authentication\FileBased;
+use Common\Authentication\InMemory;
+use Common\Authentication\MySql;
+use Common\Authentication\SqLite;
+
+
 
 /**
  * Class AuthController
@@ -23,11 +29,31 @@ class AuthController extends Controller
      *
      * @access public
      */
+
     public function action()
     {
         $postData = $this->request->getPost();
 
-        echo 'Authenticate the above two different ways' . var_dump($postData);
+
+       //echo 'Authenticate the above two different ways' . var_dump($postData);
+        if($postData->authType ==="memory") {
+            $authorize = new InMemory();
+            $authorize->authenticate($postData->username, $postData->password);
+        }
+        if($postData->authType ==="file") {
+            $authorize = new FileBased();
+            $authorize->authenticate($postData->username, $postData->password);
+        }
+        if($postData->authType ==="db") {
+            $authorize = new MySql();
+            $authorize->authenticate($postData->username, $postData->password);
+        }
+        if($postData->authType ==="lite") {
+            $authorize = new SqLite();
+            $authorize->authenticate($postData->username, $postData->password);
+        }
+
+        //$authorize->authenticate($postData->username, $postData->password);
 
         // example code: $auth = new Authentication($postData['username'], $postData['password']);
     }
--- src/Views/LoginForm.php
+++ src/Views/LoginForm.php
@@ -15,10 +15,15 @@ class LoginForm extends View
         <title>Example Login Form</title>
     </head>
     <body>
+    <link rel="stylesheet" type="text/css" href="style.css">
         <div align="center">
             <form method="POST" action="/auth">
                 Username: <input type="text" name="username" size="15" /><br />
                 Password: <input type="password" name="password" size="15" /><br />
+                <input type="radio" name="authType" value="file" checked>File Based
+                <input type="radio" name="authType" value="memory" >In Memory
+                <input type="radio" name="authType" value="db" >Data Base
+                <input type="radio" name="authType" value="lite" >SQLite
                 <p><input type="submit" value="Login" /></p>
             </form>
         </div>
